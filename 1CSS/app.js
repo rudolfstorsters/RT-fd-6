c42b69a8ea792873b2e62794981c0dd38b2115e7
rudolfstorsters <rudolfs.torsters@gmail.com>
2020-11-29 05:48:19 +0200

    few fixes

--- 1CSS/app.js
+++ 1CSS/app.js
@@ -1,32 +1,32 @@
-
 function noIdeaModeFunction() {
     var element = document.body;
     element.classList.toggle("noIdea-mode");
-  }
+}
 
-  function countFunction() {
+function countFunction() {
     var count = document.getElementById("myList").childElementCount;
     document.getElementById("demo").innerHTML = count;
-    if (count == 5) {
-      document.getElementById("addButton").style.display = "none"
-    } else {
-      document.getElementById("addButton").style.display = "block"
-    }
-  }
+    if (count == 7) {
 
-  function addFunction() {
+        var count = document.createElement("P");
+        count.innerHTML = "list is full";
+        document.getElementById("demo").appendChild(count);
+        document.getElementById("addButton").style.display = "none"
+    } else {
+        document.getElementById("addButton").style.display = "block"
+    }
+}
+
+function addFunction() {
     var newNode = document.createElement("li");
     var textnode = document.createTextNode("Water");
     newNode.appendChild(textnode);
     document.getElementById("myList").appendChild(newNode);
-  }
+}
 
-  function removeFunction() {
+function removeFunction() {
     var list = document.getElementById("myList");
     if (list.hasChildNodes()) {
-      list.removeChild(list.childNodes[0]);
+        list.removeChild(list.childNodes[0]);
     }
-  }
-
-
-
+}
